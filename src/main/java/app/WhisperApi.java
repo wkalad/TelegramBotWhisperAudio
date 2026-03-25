7f380a8a4e878a23b0b9d3f9cb8c6023b42d2d0d
Walid Lazaoui El Kharroubi <walidlazaoui1997@gmail.com>
2026-03-25 14:13:52 +0100

    Changed Output of Whisper to Json

--- src/main/java/app/WhisperApi.java
+++ src/main/java/app/WhisperApi.java
@@ -1,9 +1,14 @@
 package app;
 
+import com.google.gson.Gson;
+import dto.WhisperResult;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class WhisperApi {
 
@@ -19,10 +24,10 @@ public class WhisperApi {
 
     public String transcribe(Path path) throws IOException, InterruptedException {
 
-        builder.command("cmd.exe", "/c", "whisper " + path.toAbsolutePath() + " --model small --language Castilian --output_format txt --output_dir .\\audios");
+        builder.command("cmd.exe", "/c", "whisper " + path.toAbsolutePath() + " --model small --language Castilian --output_format json --output_dir .\\audios");
 
         Process process = builder.start();
-
+        /*
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(process.getInputStream()));
         String text = "";
@@ -31,10 +36,23 @@ public class WhisperApi {
             System.out.println(line);
             text = text + line;
         }
+        */
 
         int exitCode = process.waitFor();
 
-        return text;
+        if(exitCode != 0){
+            throw new RuntimeException("Whisper Error, Code: " + exitCode);
+        }
+
+        Path jsonPath = Paths.get(".\\audios", "audio.json");
+
+        String jsonContent = Files.readString(jsonPath);
+
+        Gson gson = new Gson();
+
+        WhisperResult whisperResult = gson.fromJson(jsonContent, WhisperResult.class);
+
+        return whisperResult.text();
 
 
     }
