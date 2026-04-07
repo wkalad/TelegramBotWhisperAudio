1bbbb0f36154a8256401a61d70a239ef41bb53d1
Walid Lazaoui El Kharroubi <walidlazaoui1997@gmail.com>
2026-04-07 18:00:27 +0200

    Created the dto to obtain the language of the user.

---
+++ src/main/java/dto/From.java
@@ -0,0 +1,4 @@
+package dto;
+
+public record From(String language_code) {
+}
--- src/main/java/dto/Message.java
+++ src/main/java/dto/Message.java
@@ -1,4 +1,4 @@
 package dto;
 
-public record Message(String text, Chat chat, Voice voice, Long message_id) {
+public record Message(String text, Chat chat, Voice voice, Long message_id, From from) {
 }
